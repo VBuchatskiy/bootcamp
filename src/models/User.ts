fce0a45fb82dd08ea7f7ee01df6bc277f1550aa0
vladimir.buchatskyi <vladimirbuchatskiy@gmail.com>
2022-10-08 19:13:24 +0300

    user register

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,17 @@
 import { Schema, model } from "mongoose";
 import { IUser } from "./types";
+import { genSalt, hash } from "bcrypt"
 
 const UserSchema = new Schema<IUser>({
   name: {
     type: String,
     trim: true,
-    unique: true,
     required: [true, 'Pleas provide a name'],
     maxlength: [50, 'Name can`t be more than 50 characters']
   },
   email: {
     type: String,
+    unique: true,
     trim: true,
     required: [true, 'Pleas provide an email']
   },
@@ -42,6 +43,13 @@ const UserSchema = new Schema<IUser>({
     createdAt: 'create_at',
     updatedAt: 'update_at'
   }
+})
+
+// @desc hash password before save
+UserSchema.pre('save', async function (next) {
+  const salt = await genSalt(10)
+  this.password = await hash(this.password, salt)
+  next()
 })
 
 export const User = model('User', UserSchema)
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,3 +1,6 @@
 import { Router } from 'express'
+import { register } from '@/controllers/auth'
 
 export const auth = Router()
+
+auth.route('/register').post(register)
