574e411c4b8cc0380aad3fd2cfa70c92efd94118
vladimir.buchatskyi <vladimirbuchatskiy@gmail.com>
2022-11-03 14:00:50 +0200

    security

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express, { json } from 'express'
+import sanitize from 'express-mongo-sanitize'
+import limit from 'express-rate-limit'
 import cors from 'cors'
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import hpp from 'hpp'
 import chalk from 'chalk'
-import { connect } from '~/config'
 import { logger, error } from "@/middleware";
 import { auth, bootcamps, courses } from '@/routers'
+import { connect } from '~/config'
 
 const app = express()
 
@@ -15,8 +20,28 @@ if (process.env.NODE_ENV === 'development') {
   app.use(logger)
 }
 
-app.use(cors())
 app.use(json())
+
+// no sql injection
+app.use(sanitize())
+
+// set security headers
+app.use(helmet())
+
+// xss clean
+app.use(xss())
+
+// rate limit
+app.use(limit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+}))
+
+// prevent http param pollution
+app.use(hpp())
+
+// enable CORS
+app.use(cors())
 
 // Mount Routes
 app.use('/api/v1/auth', auth)
